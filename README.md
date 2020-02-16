e357aa720a40879f6acec97c02e7cd5f640a804c
Tribeam <57662310+Tribeam@users.noreply.github.com>
2020-02-16 04:42:47 -0600

    Init Commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+# Lexicon Wad Extractor Thing
---
+++ src/main.lua
@@ -0,0 +1,16 @@
+
+function love.load(arg)
+
+    -- Zerobrane debugging
+    if arg[#arg] == "-debug" then
+        require("mobdebug").start()
+    end
+
+
+end
+
+function love.update(dt)
+
+end
+
+
