9e84d853e6850ceb7c45f792ecb47376658b22c0
cubebert-github <cubebertoffical@gmail.com>
2025-03-16 17:13:53 -0700

    Updated OTEX flat fix

--- src/clsWad.lua
+++ src/clsWad.lua
@@ -2331,18 +2331,32 @@ function wad:processCommonMapData(map)
             tag = love.data.unpack("<H", map.raw.sectors, s+24)
         }
 
-        -- otex flat fix
+        -- otex floor flat fix
         for i = 1, #map.sectors do
             for f = 1, #self.flats do
                 if self.flats[f].name == map.sectors[i].floor_texture then
                     local flat = self.flats[f]
                     if otex:checkImageExists(flat.name, flat.md5) then
-                        utils:printf(1, "\tFixing OTEX flat name for sector %d: %s to %s", i, map.sectors[i].floor_texture, flat.newname)
+                        utils:printf(3, "\tFixing OTEX floor flat name for sector %d: %s to %s", i, map.sectors[i].floor_texture, flat.newname)
                         map.sectors[i].floor_texture = "0" .. map.sectors[i].floor_texture:sub(2)
                     end
                 end
             end
         end
+
+        -- otex ceiling flat fix
+        for i = 1, #map.sectors do
+            for f = 1, #self.flats do
+                if self.flats[f].name == map.sectors[i].ceiling_texture then
+                    local flat = self.flats[f]
+                    if otex:checkImageExists(flat.name, flat.md5) then
+                        utils:printf(3, "\tFixing OTEX ceiling flat name for sector %d: %s to %s", i, map.sectors[i].ceiling_texture, flat.newname)
+                        map.sectors[i].ceiling_texture = "0" .. map.sectors[i].ceiling_texture:sub(2)
+                    end
+                end
+            end
+        end
+
     end
 end
 
